06a9464a86b92a4d1be39903a28c46dce6866d3d
aali <aali@winfooz.com>
2018-10-23 21:47:16 +0300

    create store

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { initialState } from './initialState'
+import { createStore } from 'redux'
+
+const store = createStore(initialState)
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
